2e1c1a7164116fb4e7d67657af41f2a3426f51e9
ESLAM-MO <eslammo@gmail.com>
2026-01-30 11:36:21 +0200

    security: move secrets to environment variables

--- orbit/settings.py
+++ orbit/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kf5bxj(&3h%3r8&41mphgj_%##k^2a$=rt6(3wmd1x#o$cfmdz'
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key-for-dev")
 
-ALLOWED_HOSTS = []
+DEBUG = os.getenv("DEBUG", "True") == "True"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+
 
 
 # Application definition
